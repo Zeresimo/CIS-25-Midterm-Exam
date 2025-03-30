cdee35e152db9cb61ae9745621c2023815fc7e31
Leo Castillo <castillo.leorenzo@gmail.com>
2025-03-29 19:59:41 -0700

    Part 1 Functions
    First part of the assignment

---
+++ Part1_Functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// Receives float values for length and width, and returns the area of a rectangle
+calculateArea(float length, float width)
+{
+    return length * width; // Area = length * width
+}
+
+// Checks if given number is a prime number
+// “The smallest factor of a number greater than one cannot be greater than the square root of that number.”
+isPrime(int number)
+{
+    if (number <= 1) // Prime numbers are greater than 1
+        return false;
+    for (int i = 2; i <= sqrt(number); i++) // Checks if the number is divisible by any number from 2 to the square root of the number
+    {
+        if (number % i == 0) // If the number is divisible by i, it is not prime
+            return false;
+    }
+    return true; // If no divisors were found, it is prime
+}
